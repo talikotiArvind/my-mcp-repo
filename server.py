5e43f58187c57041f0be6f984b070fe7ebfdf050
Arvind Talikoti <139647225+talikotiArvind@users.noreply.github.com>
2025-11-25 14:49:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,12 @@
+from fastmcp import FastMCP
 
+
+app = FastMCP()
+
+
+@app.tool
+def greetings_tool(name: str) -> str:
+  return f"Hello {name}, This is welcome from the MCP server of github"
+
+if __name__ == "__main__":
+    app.run()
